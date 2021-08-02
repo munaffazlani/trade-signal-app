4b29b46fe1fc1474b864f03e662efd8f7edca662
Samarth <samarth@creolestudios.com>
2021-08-02 21:53:52 +0530

    added config package name

--- app.config.js
+++ app.config.js
@@ -1,9 +1,12 @@
 export default {
   name: "Trading Patterns & Breakouts Signals",
   version: "1.0.0",
+  android: {
+    package: "com.munaffazlani.tradingpatternsandbreakoutssignals",
+  },
   extra: {
     androidStandaloneAppClientId:
-      process.env.ANDROID_STANDALONE_APP_CLIENT_ID =
-      "344342272741-bu36tf36hn5o3jrofhad3ps6e8ooqt0e.apps.googleusercontent.com",
+      (process.env.ANDROID_STANDALONE_APP_CLIENT_ID ===
+        "344342272741-bu36tf36hn5o3jrofhad3ps6e8ooqt0e.apps.googleusercontent.com"),
   },
 };
