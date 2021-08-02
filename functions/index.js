0b615de879d82425adeaf9b2ff6dfc8b110a4298
Samarth <samarth@creolestudios.com>
2021-08-02 21:51:10 +0530

    added cloud function codes

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,44 @@
 const functions = require("firebase-functions");
 const fetch = require("node-fetch");
 const admin = require("firebase-admin");
+const {firestore} = require("firebase-admin");
 
 admin.initializeApp(functions.config().firebase);
+
+exports.sendPushNotification = functions.firestore
+    .document("groups/{groupId}/allSignals/{signalId}")
+    .onCreate((snap, context) => {
+      const groupId = context.params.groupId;
+      const newSignal = snap.data();
+      const messages = [];
+      console.log(newSignal, "newSignal");
+      firestore()
+          .collection("groups")
+          .doc(groupId)
+          .collection("subscriberTokens")
+          .where("mute", "==", 0)
+          .get()
+          .then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+              // doc.data() is never undefined for query doc snapshots
+              console.log(doc.id, " => ", doc.data());
+              messages.push({
+                "to": doc.id,
+                "title": newSignal.title,
+                "body": newSignal.description,
+              });
+            });
+          })
+          .catch((error) => {
+            console.log("Error getting documents: ", error);
+          });
+      fetch("https://exp.host/--/api/v2/push/send", {
+        method: "POST",
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json",
+
+        },
+        body: JSON.stringify(messages),
+      });
+    });
